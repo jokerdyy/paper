87ebe567a493e3041a4af2906319d297558533f7
jokerdyy <1158447564@qq.com>
2022-03-28 19:52:06 +0800

    v1.0

---
+++ tuxiang3.m
@@ -0,0 +1,19 @@
+figure;
+a=xlsread('C:\Users\Administrator\Desktop\机械臂角度仿真数据\画图.xlsx','sheet1');
+x1=a(:,1);
+y1=a(:,2);
+z1=a(:,8);
+Q=plot3(x1,y1,z1,'k');
+hold on
+x2=a(:,1);
+y2=a(:,2);
+z2=a(:,9);
+Y=plot3(x2,y2,z2,'p');
+set(Y,'LineWidth',1.5); 
+xlabel('Deviation angle');
+ylabel('distance');
+zlabel('angle of θ2');
+set(Q,'LineWidth',1.5); 
+set(gca,'FontSize',10);
+legend('Theoretical angle','Actual angle');
+title('Elbow angle scatter plot')
